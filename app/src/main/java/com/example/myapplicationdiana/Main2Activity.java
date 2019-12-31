cb1c6818653e42ad136cf997cef3f78a3b229e62
Diana <dianaonce99@gmail.com>
2019-12-31 15:55:54 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/myapplicationdiana/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.myapplicationdiana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+    private EditText et1,et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
